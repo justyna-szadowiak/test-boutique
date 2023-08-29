cc3606f0a43d2c2e6b8cb9ede4cb1df7fa08c7ba
Justyna <justyna.elzbieta.szadowiak@gmail.com>
2023-08-29 14:18:38 +0200

    Test for checking issue of downloading PDF

--- cypress/e2e/main.cy.js
+++ cypress/e2e/main.cy.js
@@ -4,43 +4,60 @@ const header = "PrecisionTestPro produtcs and information";
 const buttonWiki = "Read on wikipedia";
 
 describe(`Application Test in main.ts . Main file`, () => {
-    it('Should open the application URL, finds dialog modal and close it.', () => {
+    it('Should open the application URL, and go through whole page.', () => {
+        //Testing is URL correct
         cy.visit(URL);
+        //Test should caught dialog modal, and after finding it, close it
         cy.get('[data-cy="dialog-modal"]').within(() => {
             cy.get('[data-cy="paragraph-modal"]').should('contains.text', paragraph);
             cy.get('[data-cy="close-modal"]').click({multiple: true});
         });
+        //Test should check GET request
         cy.request('GET', URL).then(response => {
             expect(response.status).to.eq(200);
             expect(response).to.have.property('headers');
         });
+        cy.wait(3000)
+        //Test should check is the header correct if not will return error in log
         cy.get('[data-cy="app-header"]').then((value) => {
             if(value) {
                 header
             } else {
-                cy.log('The header is not correctly.')
+                cy.log('ERROR: The header is not correctly.')
             }
+            cy.wait(3000)
         });
+        //Test should check is the image displaying correctly, if not return error in log
         cy.get('[data-cy="product-image"]').then((image) => {
             if(image) {
                 cy.log('product.jpg');
             } else {
-                cy.log('The image is not displaying correctly.');
+                cy.log('ERROR: The image is not displaying correctly.');
             }
+            cy.wait(3000)
         })
+        //Test should check the link for downloading pdf file and go back 
         cy.get('[data-cy="links"]')
             .first()
             .within(() => {
-            cy.get('[data-cy="link-pdf"]').click();
-            cy.wait(2000)
+            cy.get('[data-cy="link-pdf"]').then((pdf) => {
+                if(pdf) {
+                    cy.contains('Download PDF').click();
+                    cy.log('ERROR: Cannot GET /products/brochure.pdf');
+                    cy.wait(3000);
+                } else {
+                    cy.contains('Download PDF').click();
+                    cy.readFile('brochure.pdf');
+                }
+            });
         });
         cy.go('back');
         cy.get('[data-cy="link-wiki"]')
             .first()
             .within(() => {
                 cy.contains(buttonWiki).click();
-                cy.wait(1000)
+                cy.wait(2000)
         });
-        
+        cy.go(-2)
     });
 });
